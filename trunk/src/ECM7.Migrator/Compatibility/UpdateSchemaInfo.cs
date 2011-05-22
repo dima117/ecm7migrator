868309935c1d4db53c0108b87fdaaccdc2d49a4b
kvsoft98 <kvsoft98@ba760414-8989-11de-8f56-7b2e0289298d>
2011-05-22 16:41:46 +0000

    A separate class was created for compatibility code

---
+++ trunk/src/ECM7.Migrator/Compatibility/UpdateSchemaInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ECM7.Migrator.Framework;
+using System.Data;
+
+namespace ECM7.Migrator.Compatibility
+{
+	class UpdateSchemaInfo
+	{
+		/// <summary>
+		/// Расширяет таблицу SchemaInfo полем Key.
+		/// Использутеся при переходе от старой версии
+		/// 
+		/// Должно быть удалено в будущем
+		/// </summary>
+		/// <param name="provider"></param>
+		public static void Update(ITransformationProvider provider)
+		{
+			provider.AddTable(
+				"SchemaTmp",
+				new Column("Version", DbType.Int64, ColumnProperty.NotNull),
+				new Column("[Key]", DbType.String.WithSize(200), ColumnProperty.NotNull, "''"));
+			provider.ExecuteNonQuery("INSERT INTO SchemaTmp (Version) SELECT Version FROM SchemaInfo");
+			provider.RemoveTable("SchemaInfo");
+			provider.RenameTable("SchemaTmp", "SchemaInfo");
+			provider.AddUniqueConstraint("UC_SchemaInfo", "SchemaInfo", "Version", "[Key]");
+		}
+	}
+}
