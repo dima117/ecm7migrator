ed73ff2fa7cc4639b89746876e7b72bb9caca3d9
dima117a <dima117a@ba760414-8989-11de-8f56-7b2e0289298d>
2011-07-15 20:25:07 +0000

    - format provider для экранирования зарезервированных слов в идентификаторах;

---
+++ trunk/src/ECM7.Migrator.Tests2/SqlFormatterTest.cs
@@ -0,0 +1,30 @@
+﻿namespace ECM7.Migrator.Tests2
+{
+	using System;
+
+	using ECM7.Migrator.Providers;
+
+	using NUnit.Framework;
+
+	/// <summary>
+	/// TODO: Update summary.
+	/// </summary>
+	[TestFixture]
+	public class SqlFormatterTest
+	{
+		private static string Convert(string arg)
+		{
+			return "<{0}>".FormatWith(arg);
+		}
+
+		private static SqlFormatter formatter = new SqlFormatter(Convert);
+
+
+		[Test]
+		public void FormatterTest()
+		{
+			string sql = "insert into {0:TAB} set {1:COL} = '{2}'".FormatWith(formatter, "test1", "column1", "value1");
+			Assert.AreEqual(sql, "insert into <test1> set <column1> = 'value1'");
+		}
+	}
+}
