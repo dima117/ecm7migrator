273f33aa6598048a369e58c5f6b0b51fd9178be9
dima117a <dima117a@ba760414-8989-11de-8f56-7b2e0289298d>
2011-09-05 08:52:09 +0000

    - тесты на MSSQL CE; - исправлена ошибка в мэппинг типов MSSQL CE; - тесты сделаны виртуальными;

---
+++ src/ECM7.Migrator.Providers.Tests/ImplementationTest/SqlServerCeTransformationProviderTest.cs
@@ -0,0 +1,65 @@
+﻿namespace ECM7.Migrator.Providers.Tests.ImplementationTest
+{
+	using System;
+
+	using ECM7.Migrator.Providers.SqlServer;
+
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class SqlServerCeTransformationProviderTest
+		: TransformationProviderTestBase<SqlServerCeTransformationProvider>
+	{
+		#region Overrides of TransformationProviderTestBase<SqlServerCeTransformationProvider>
+
+		public override string ConnectionStrinSettingsName
+		{
+			get { return "SqlServerCeConnectionString"; }
+		}
+
+		protected override string BatchSql
+		{
+			get
+			{
+				return @"
+					insert into [BatchSqlTest] ([Id], [TestId]) values (11, 111)
+					GO
+					insert into [BatchSqlTest] ([Id], [TestId]) values (22, 222)
+					GO
+					insert into [BatchSqlTest] ([Id], [TestId]) values (33, 333)
+					GO
+					insert into [BatchSqlTest] ([Id], [TestId]) values (44, 444)
+					GO
+					go
+					insert into [BatchSqlTest] ([Id], [TestId]) values (55, 555)";
+			}
+		}
+
+		#endregion
+
+		#region override tests
+
+		[Test]
+		public override void CanAddCheckConstraint()
+		{
+			Assert.Throws<NotSupportedException>(() =>
+				base.CanAddCheckConstraint());
+		}
+
+		[Test]
+		public override void CanVerifyThatCheckConstraintIsExist()
+		{
+			Assert.Throws<NotSupportedException>(() =>
+				base.CanVerifyThatCheckConstraintIsExist());
+		}
+
+		[Test]
+		public override void CanRenameColumn()
+		{
+			Assert.Throws<NotSupportedException>(() =>
+				base.CanRenameColumn());
+		}
+
+		#endregion
+	}
+}
