671c6ea0a9565b3cce9701bedab5ed8ca1c8796f
dima117a <dima117a@ba760414-8989-11de-8f56-7b2e0289298d>
2011-10-23 12:57:24 +0000

    - методы Up и Down переименованы в Apply и Revert; - пустая реализация метода Revert по умолчанию;

--- src/ECM7.Migrator.TestAssembly/ThirdTestMigration.cs
+++ src/ECM7.Migrator.TestAssembly/ThirdTestMigration.cs
@@ -11,14 +11,14 @@ namespace ECM7.Migrator.TestAssembly
 		/// <summary>
 		/// Defines tranformations to port the database to the current version.
 		/// </summary>
-		public override void Up()
+		public override void Apply()
 		{
 		}
 
 		/// <summary>
-		/// Defines transformations to revert things done in <c>Up</c>.
+		/// Defines transformations to revert things done in <c>Apply</c>.
 		/// </summary>
-		public override void Down()
+		public override void Revert()
 		{
 		}
 	}
