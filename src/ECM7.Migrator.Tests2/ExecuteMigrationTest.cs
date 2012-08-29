22a367cf5b8a7c29a24873622c528ceccf5de407
dima117a <dima117a@ba760414-8989-11de-8f56-7b2e0289298d>
2012-08-29 18:45:34 +0000

    - тесты на выполнение миграций без транзакции;

--- src/ECM7.Migrator.Tests2/ExecuteMigrationTest.cs
+++ src/ECM7.Migrator.Tests2/ExecuteMigrationTest.cs
@@ -15,6 +15,8 @@
 	[TestFixture]
 	public class ExecuteMigrationTest
 	{
+		#region with transaction
+
 		/// <summary>
 		/// Проверка выполнения миграции "вверх"
 		/// </summary>
@@ -77,5 +79,68 @@
 				provider.Verify(db => db.Rollback());
 			}
 		}
+
+		#endregion
+
+		#region without transaction
+
+		/// <summary>
+		/// Проверка выполнения миграции "вверх" (без транзакции)
+		/// </summary>
+		[Test]
+		public void CanMoveUpWithoutTransaction()
+		{
+			var provider = new Mock<ITransformationProvider>();
+			Assembly asm = Assembly.Load("ECM7.Migrator.TestAssembly");
+
+			using (var migrator = new Migrator(provider.Object, asm))
+			{
+				migrator.ExecuteMigration(4, 3);
+
+				provider.Verify(db => db.ExecuteNonQuery("up4"));
+				provider.Verify(db => db.MigrationApplied(4, "test-key111"));
+			}
+		}
+
+		/// <summary>
+		/// Проверка выполнения миграции "вниз" (без транзакции)
+		/// </summary>
+		[Test]
+		public void CanMoveDownWithoutTransaction()
+		{
+			var provider = new Mock<ITransformationProvider>();
+			Assembly asm = Assembly.Load("ECM7.Migrator.TestAssembly");
+
+			using (var migrator = new Migrator(provider.Object, asm))
+			{
+				migrator.ExecuteMigration(4, 4);
+
+				provider.Verify(db => db.ExecuteNonQuery("down4"));
+				provider.Verify(db => db.MigrationUnApplied(4, "test-key111"));
+			}
+		}
+
+		/// <summary>
+		/// Проверка, что при возникновении ошибки (без транзакции) не выполняется откат транзакции
+		/// </summary>
+		[Test]
+		public void ShouldNoRollbackWhenExceptionWithoutTransaction()
+		{
+			Assembly asm = Assembly.Load("ECM7.Migrator.TestAssembly");
+			var provider = new Mock<ITransformationProvider>();
+
+			provider
+				.Setup(db => db.MigrationUnApplied(It.IsAny<long>(), It.IsAny<string>()))
+				.Throws<Exception>();
+
+			using (var migrator = new Migrator(provider.Object, asm))
+			{
+				Assert.Throws<Exception>(() => migrator.ExecuteMigration(2, 2));
+
+				provider.Verify(db => db.MigrationUnApplied(2, It.IsAny<string>()));
+			}
+		}
+
+		#endregion
 	}
 }
