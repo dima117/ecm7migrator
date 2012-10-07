bc2c5b61f0625cea22f16ff8c7d3409e7c0b5bc3
dima117a <dima117a@ba760414-8989-11de-8f56-7b2e0289298d>
2012-10-07 19:39:58 +0000

    - у класса SqlRunner удален метод Finalize;

--- trunk/src/ECM7.Migrator.Tests2/OtherTests.cs
+++ trunk/src/ECM7.Migrator.Tests2/OtherTests.cs
@@ -1,4 +1,8 @@
-﻿using ECM7.Migrator.Framework;
+﻿using System;
+using System.Configuration;
+using ECM7.Migrator.Framework;
+using ECM7.Migrator.Providers;
+using ECM7.Migrator.Providers.SqlServer;
 using NUnit.Framework;
 
 namespace ECM7.Migrator.Tests2
@@ -12,6 +16,17 @@ namespace ECM7.Migrator.Tests2
 			Assert.AreEqual(
 			"Migration0101 add new table with primary key",
 			StringUtils.ToHumanName("Migration0101_Add_NewTable_with_primary___Key"));
+		}
+
+		[Test]
+		public void ProviderFinalizeTest()
+		{
+			string cstring = ConfigurationManager.AppSettings["SqlServerConnectionString"];
+			var provider = ProviderFactory.Create(typeof (SqlServerTransformationProvider), cstring, null);
+			provider.ExecuteScalar("select 1");
+
+			provider = null;
+			GC.Collect();
 		}
 	}
 }
