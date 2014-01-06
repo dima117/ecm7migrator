e9b7ef1dc9b3a588089eab363d900165a79c1237
dima117a <dima117a@ba760414-8989-11de-8f56-7b2e0289298d>
2014-01-06 21:10:08 +0000

    - исправление генерации SQL для MSSQL;

---
+++ src/ECM7.Migrator.Providers.Tests/ImplementationTest/NoQuotesWithSchema/SqlServerTransformationProviderSchemaTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace ECM7.Migrator.Providers.Tests.ImplementationTest.NoQuotesWithSchema
+{
+	[TestFixture, Category("SqlServer")]
+	public class SqlServerTransformationProviderSchemaTest : WithSchema.SqlServerTransformationProviderSchemaTest
+	{
+		protected override bool AddQuotes
+		{
+			get { return false; }
+		}
+	}
+}
