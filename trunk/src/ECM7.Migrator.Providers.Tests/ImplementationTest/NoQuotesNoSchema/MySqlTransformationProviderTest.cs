ba7492b8518954545c516a8f8484652bec79338b
dima117a <dima117a@ba760414-8989-11de-8f56-7b2e0289298d>
2014-01-09 18:54:33 +0000

    - заготовка для тестов mysql без экранирования идентификаторов; - исправлена генерация sql для удаления первичного ключа;

---
+++ trunk/src/ECM7.Migrator.Providers.Tests/ImplementationTest/NoQuotesNoSchema/MySqlTransformationProviderTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace ECM7.Migrator.Providers.Tests.ImplementationTest.NoQuotesNoSchema
+{
+	[TestFixture, Category("MySql")]
+	public class MySqlTransformationProviderTest
+        : NoSchema.MySqlTransformationProviderTest
+	{
+        protected override bool AddQuotes
+        {
+            get { return false; }
+        }
+	}
+}
