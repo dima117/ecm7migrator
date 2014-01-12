3d99f03bbae00bf572666940bc6a8712a0b79381
dima117a <dima117a@ba760414-8989-11de-8f56-7b2e0289298d>
2014-01-12 20:50:56 +0000

    - тесты mysql без экранирования идентификаторов;

---
+++ trunk/src/ECM7.Migrator.Providers.Tests/ImplementationTest/NoQuotesWithSchema/MySqlTransformationProviderSchemaTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace ECM7.Migrator.Providers.Tests.ImplementationTest.NoQuotesWithSchema
+{
+	[TestFixture, Category("MySql")]
+    public class MySqlTransformationProviderSchemaTest : WithSchema.MySqlTransformationProviderSchemaTest
+	{
+	    protected override bool AddQuotes
+	    {
+	        get { return false; }
+	    }
+	}
+}
