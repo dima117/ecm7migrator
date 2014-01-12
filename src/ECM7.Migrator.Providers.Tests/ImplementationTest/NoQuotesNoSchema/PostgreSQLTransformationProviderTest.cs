f4dd2daed6a2e46b58789a0b2f860586c878a100
dima117a <dima117a@ba760414-8989-11de-8f56-7b2e0289298d>
2014-01-12 21:28:41 +0000

    - тесты npgsql без экранирования идентификаторов;

---
+++ src/ECM7.Migrator.Providers.Tests/ImplementationTest/NoQuotesNoSchema/PostgreSQLTransformationProviderTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace ECM7.Migrator.Providers.Tests.ImplementationTest.NoQuotesNoSchema
+{
+	[TestFixture, Category("PostgreSQL")]
+	public class PostgreSQLTransformationProviderTest
+		: NoSchema.PostgreSQLTransformationProviderTest
+	{
+		protected override bool IgnoreCase
+		{
+			get { return true; }
+		}
+
+		protected override bool AddQuotes
+		{
+			get { return false; }
+		}
+	}
+}
